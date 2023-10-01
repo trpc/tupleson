32720a445acfcf31d51caa38dc9d7c7a165771d3
Alex / KATT <alexander@n1s.se>
2023-10-01 16:31:56 +0200

    feat: simplify symbols (#16)

--- src/handlers/tsonSymbol.ts
+++ src/handlers/tsonSymbol.ts
@@ -1,15 +1,13 @@
 import { TsonType } from "../types.js";
 
-const symbols: symbol[] = [];
 export const tsonSymbol = <T extends symbol>(
 	symbol: T,
-): TsonType<T, number> => {
-	symbols.push(symbol);
-	const index = symbols.length - 1;
+): TsonType<T, string> => {
+	const key = symbol.toString();
 	return {
-		deserialize: (index) => symbols[index] as T,
-		key: `Symbol${index}`,
-		serialize: () => index,
+		deserialize: () => symbol,
+		key,
+		serialize: () => key,
 		test: (v) => v === symbol,
 	};
 };
