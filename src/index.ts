d153d76f2d4d67c324f5b305166101e7336fccde
KATT <alexander@n1s.se>
2023-10-03 11:09:25 +0200

    cool

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 // async
 export { createTsonAsync } from "./async/createTsonAsync.js";
 export { createAsyncTsonStringify } from "./async/serializeAsync.js";
+
+// sync
 export { createTson } from "./sync/createTson.js";
 export { createTsonDeserialize, createTsonParser } from "./sync/deserialize.js";
 export { createTsonSerialize, createTsonStringify } from "./sync/serialize.js";
 export * from "./handlers/index.js";
 
+// types
 export type { TsonType } from "./types.js";
 export type { TsonOptions } from "./types.js";
