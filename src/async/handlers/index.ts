6c0a508a5969b954bb794bba9cdea1366cea44bc
KATT <alexander@n1s.se>
2023-10-07 14:08:04 +0200

    only 1 index file

--- src/async/handlers/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./tsonPromise.js";
-export * from "./tsonAsyncIterable.js";
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,32 @@
-// sync
+// --- sync --
 export { createTson } from "./sync/createTson.js";
 export { createTsonDeserialize, createTsonParser } from "./sync/deserialize.js";
 export { createTsonSerialize, createTsonStringify } from "./sync/serialize.js";
-export * from "./sync/handlers/index.js";
+export type { TsonType } from "./sync/syncTypes.js";
+export type { TsonOptions } from "./sync/syncTypes.js";
 
-// async
+// type handlers
+export * from "./sync/handlers/tsonBigint.js";
+export * from "./sync/handlers/tsonDate.js";
+export * from "./sync/handlers/tsonRegExp.js";
+export * from "./sync/handlers/tsonSet.js";
+export * from "./sync/handlers/tsonMap.js";
+export * from "./sync/handlers/tsonUndefined.js";
+export * from "./sync/handlers/tsonUnknownObjectGuard.js";
+export * from "./sync/handlers/tsonNumberGuard.js";
+export * from "./sync/handlers/tsonURL.js";
+export * from "./sync/handlers/tsonSymbol.js";
+
+// --- async --
+export type { TsonAsyncOptions } from "./async/asyncTypes.js";
+
 export { createTsonAsync } from "./async/createTsonAsync.js";
 export { createTsonParseAsync } from "./async/deserializeAsync.js";
 export {
 	createAsyncTsonSerialize,
 	createTsonStringifyAsync,
 } from "./async/serializeAsync.js";
-export * from "./async/handlers/index.js";
 
-// types
-export type { TsonAsyncOptions } from "./async/asyncTypes.js";
-export type { TsonType } from "./sync/syncTypes.js";
-export type { TsonOptions } from "./sync/syncTypes.js";
+// type handlers
+export * from "./async/handlers/tsonAsyncIterable.js";
+export * from "./async/handlers/tsonPromise.js";
