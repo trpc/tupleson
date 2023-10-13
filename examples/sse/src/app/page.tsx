0477cc1ae5b11ca58144ab2f758bd5ab6e6d0f29
KATT <alexander@n1s.se>
2023-10-14 00:03:25 +0200

    mono

--- examples/sse/src/app/page.tsx
+++ examples/sse/src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
 	return (
 		<section className="flex h-screen justify-center items-center bg-gray-100 dark:bg-gray-800">
 			<div className="bg-white dark:bg-gray-900 p-8 rounded-xl shadow-md">
-				<h1 className="text-2xl text-center text-gray-800 dark:text-gray-200">
+				<h1 className="text-2xl text-center text-gray-800 dark:text-gray-200 font-mono">
 					<StreamedTime />
 				</h1>
 			</div>
