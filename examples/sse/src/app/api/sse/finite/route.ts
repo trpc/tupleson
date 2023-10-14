35850ebc94948d151405784d67080880d2a8cef5
KATT <alexander@n1s.se>
2023-10-14 15:52:42 +0200

    make the ui a bit clearer

--- examples/sse/src/app/api/sse/finite/route.ts
+++ examples/sse/src/app/api/sse/finite/route.ts
@@ -6,10 +6,10 @@ const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
  * This function returns the object we will be sending to the client.
  */
 export function getResponseShape() {
-	const tuple = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+	let i = 0;
 	async function* finiteListGenerator() {
-		while (tuple.length) {
-			yield tuple.shift();
+		while (i < 10) {
+			yield i++;
 			await sleep(500);
 		}
 	}
