aa2cbad179985c3204e52c9482b480694acbb426
Alex / KATT <alexander@n1s.se>
2023-10-20 16:22:08 +0200

    feat: handle reconnects when response is interrupted (#77)
    - add option to resume interrupted streams
    - we assume that any reconnect will return a result with the same shape
    
    ## Notes
    
    
    - it's a bit mind-bending try to to follow exactly what happens, but the
    test seems to pass
    - we'll likely use SSE for subscriptions in tRPC and SSE _or_ JSON
    streams for query/mutation results
    - for subscriptions we'll have `reconnect:true` in our client

--- src/async/sse.test.ts
+++ src/async/sse.test.ts
@@ -1,9 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unnecessary-condition */
 import { EventSourcePolyfill, NativeEventSource } from "event-source-polyfill";
-import { expect, test } from "vitest";
+import { expect, test, vi } from "vitest";
 (global as any).EventSource = NativeEventSource || EventSourcePolyfill;
 
-import { TsonAsyncOptions, tsonAsyncIterable, tsonPromise } from "../index.js";
+import {
+	TsonAsyncOptions,
+	tsonAsyncIterable,
+	tsonBigint,
+	tsonPromise,
+} from "../index.js";
 import { createTestServer, sleep } from "../internals/testUtils.js";
 import { createTsonAsync } from "./createTsonAsync.js";
 
@@ -13,15 +18,12 @@ test("SSE response test", async () => {
 			let i = 0;
 			while (true) {
 				yield i++;
-				await sleep(100);
+				await sleep(10);
 			}
 		}
 
 		return {
-			foo: "bar",
 			iterable: generator(),
-			promise: Promise.resolve(42),
-			rejectedPromise: Promise.reject(new Error("rejected promise")),
 		};
 	}
 
@@ -73,14 +75,14 @@ test("SSE response test", async () => {
 		});
 
 		expect(messages).toMatchInlineSnapshot(`
-		[
-		  "{\\"json\\":{\\"foo\\":\\"bar\\",\\"iterable\\":[\\"AsyncIterable\\",0,\\"__tson\\"],\\"promise\\":[\\"Promise\\",1,\\"__tson\\"],\\"rejectedPromise\\":[\\"Promise\\",2,\\"__tson\\"]},\\"nonce\\":\\"__tson\\"}",
-		  "[0,[0,0]]",
-		  "[1,[0,42]]",
-		  "[2,[1,{}]]",
-		  "[0,[0,1]]",
-		]
-	`);
+			[
+			  "{\\"json\\":{\\"iterable\\":[\\"AsyncIterable\\",0,\\"__tson\\"]},\\"nonce\\":\\"__tson\\"}",
+			  "[0,[0,0]]",
+			  "[0,[0,1]]",
+			  "[0,[0,2]]",
+			  "[0,[0,3]]",
+			]
+		`);
 	}
 
 	{
@@ -109,4 +111,100 @@ test("SSE response test", async () => {
 			]
 		`);
 	}
+});
+
+test("handle reconnects - iterator wrapped in Promise", async () => {
+	let i = 0;
+
+	let kill = false;
+	function createMockObj() {
+		async function* generator() {
+			while (true) {
+				await sleep(10);
+				yield BigInt(i);
+				i++;
+
+				if (i % 5 === 0) {
+					kill = true;
+				}
+
+				if (i > 11) {
+					// done
+					return;
+				}
+			}
+		}
+
+		return {
+			iterable: Promise.resolve(generator()),
+		};
+	}
+
+	type MockObj = ReturnType<typeof createMockObj>;
+
+	// ------------- server -------------------
+	const opts = {
+		nonce: () => "__tson" + i, // add index to nonce to make sure it's not cached
+		types: [tsonPromise, tsonAsyncIterable, tsonBigint],
+	} satisfies TsonAsyncOptions;
+
+	const server = await createTestServer({
+		handleRequest: async (_req, res) => {
+			const tson = createTsonAsync(opts);
+
+			const obj = createMockObj();
+			const response = tson.toSSEResponse(obj);
+
+			for (const [key, value] of response.headers) {
+				res.setHeader(key, value);
+			}
+
+			for await (const value of response.body as any) {
+				res.write(value);
+				if (kill) {
+					// interrupt the stream
+					res.end();
+					kill = false;
+					return;
+				}
+			}
+
+			res.end();
+		},
+	});
+
+	// ------------- client -------------------
+	const tson = createTsonAsync(opts);
+
+	// e2e
+	const ac = new AbortController();
+	const onReconnect = vi.fn();
+	const shape = await tson.createEventSource<MockObj>(server.url, {
+		onReconnect,
+		reconnect: true,
+		signal: ac.signal,
+	});
+
+	const messages: bigint[] = [];
+
+	for await (const value of await shape.iterable) {
+		messages.push(value);
+	}
+
+	expect(messages).toMatchInlineSnapshot(`
+		[
+		  0n,
+		  1n,
+		  2n,
+		  3n,
+		  4n,
+		  6n,
+		  7n,
+		  8n,
+		  9n,
+		  11n,
+		]
+	`);
+
+	expect(onReconnect).toHaveBeenCalledTimes(2);
 });
