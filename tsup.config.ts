98094a5c29e2ddf4afd19327160415a370ec4a41
Sachin Raja <me@s4n.land>
2023-11-04 11:08:50 -0700

    fix: rewrite bundled extensions to mjs for esm build

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,5 +7,20 @@ export default defineConfig({
 	entry: ["src/**/*.ts", "!src/**/*.test.*"],
 	format: ["cjs", "esm"],
 	outDir: "lib",
+	plugins: [
+		{
+			name: "fix-cjs",
+			renderChunk(_, chunk) {
+				if (this.format === "esm") {
+					// replace `from '...js'` with `from '...mjs'` for mjs imports & exports
+					const code = chunk.code.replace(
+						/from ['"](.*)\.js['"]/g,
+						"from '$1.mjs'",
+					);
+					return { code };
+				}
+			},
+		},
+	],
 	sourcemap: true,
 });
